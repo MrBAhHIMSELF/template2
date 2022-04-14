74582d162a20b9860c44259a1dccff3a9d0e9f78
kiaramclarty <89852907+kiaramclarty@users.noreply.github.com>
2022-04-14 12:23:57 -0400

    4/14 changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import GlobalStyle from "./globalStyles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-
+import Api from "./components/Api";
 //Pages
 import Home from "./pages/Home";
 import SignUp from "./pages/SignupPage";
@@ -10,10 +10,24 @@ import Pricing from "./pages/PricingPage";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  componentDidMount = () => {
+    fetch(
+      "https://data.cityofnewyork.us/resource/rsgh-akpg.json?$$app_token=3yrs3ewX0YYxoOpKKiJ0aWIxW"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data });
+        console.log(this.state.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   return (
     <Router>
-      <GlobalStyle/>
+      <GlobalStyle />
       <Navbar />
+      <Api />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={SignUp} />
